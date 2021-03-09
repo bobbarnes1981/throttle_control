b4aa03070150f06ed1f846cb73cf087fda9d3e85
Robert Barnes <robertbarnes1981@googlemail.com>
2021-03-09 10:58:49 +0000

    Write calibration to flash

--- throttle_control.ino
+++ throttle_control.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 
 #define app0 A0 // accelerator pedal position #0
 #define app1 A1 // accelerator pedal position #1
@@ -5,17 +6,31 @@
 #define pwm 9   // motor speed output
 #define dir 10  // motor direction control
 
-#define app0min 0
-#define app0max 1023
-#define app1min 0
-#define app1max 1023
-#define tps0min 0
-#define tps0max 1023
+#define app0min_LOWBYTE 0
+#define app0min_HIGHBYTE 1
+#define app0max_LOWBYTE 2
+#define app0max_HIGHBYTE 3
+#define app1min_LOWBYTE 4
+#define app1min_HIGHBYTE 5
+#define app1max_LOWBYTE 6
+#define app1max_HIGHBYTE 7
+#define tps0min_LOWBYTE 8
+#define tps0min_HIGHBYTE 9
+#define tps0max_LOWBYTE 10
+#define tps0max_HIGHBYTE 11
+
 #define pwmmin 0
 #define pwmmax 1999
 
 #define LED_PIN 6
 #define BTN_PIN 2
+
+int app0min = -1;
+int app0max = -1;
+int app1min = -1;
+int app1max = -1;
+int tps0min = -1;
+int tps0max = -1;
 
 int cal_app0min = -1;
 int cal_app0max = -1;
@@ -28,9 +43,60 @@ int cal_tps0max = -1;
 
 bool calibration = false;
 
-void setup() {
-  timerInitialisation();
-    
+int readIntFromEEPROM(int low, int high) {
+  int val = EEPROM.read(low) | (EEPROM.read(high)<<8);
+  #ifdef DEBUG
+//  Serial.print(low, HEX);
+//  Serial.print(' ');
+//  Serial.print(high, HEX);
+//  Serial.print(' ');
+//  Serial.println(val);
+  #endif
+  return val;
+}
+
+void readDataFromEEPROM() {
+  app0min = readIntFromEEPROM(app0min_LOWBYTE, app0min_HIGHBYTE);
+  app0max = readIntFromEEPROM(app0max_LOWBYTE, app0max_HIGHBYTE);
+
+  app1min = readIntFromEEPROM(app1min_LOWBYTE, app1min_HIGHBYTE);
+  app1max = readIntFromEEPROM(app1max_LOWBYTE, app1max_HIGHBYTE);
+
+  tps0min = readIntFromEEPROM(tps0min_LOWBYTE, tps0min_HIGHBYTE);
+  tps0max = readIntFromEEPROM(tps0max_LOWBYTE, tps0max_HIGHBYTE);
+
+  #ifdef DEBUG
+//  Serial.println(app0min);
+//  Serial.println(app0max);
+//  Serial.println(app1min);
+//  Serial.println(app1max);
+//  Serial.println(tps0min);
+//  Serial.println(tps0max);
+//  delay(10000);
+  #endif
+}
+
+void writeIntToEEPROM(int low, int high, int value) {
+  EEPROM.write(low, value & 0x00FF);
+  EEPROM.write(high, (value & 0xFF00) >> 8);
+}
+
+void writeCalibrationToEEPROM() {
+  writeIntToEEPROM(app0min_LOWBYTE, app0min_HIGHBYTE, cal_app0min);
+  writeIntToEEPROM(app0max_LOWBYTE, app0max_HIGHBYTE, cal_app0max);
+
+  writeIntToEEPROM(app1min_LOWBYTE, app1min_HIGHBYTE, cal_app1min);
+  writeIntToEEPROM(app1max_LOWBYTE, app1max_HIGHBYTE, cal_app1max);
+
+  writeIntToEEPROM(tps0min_LOWBYTE, tps0min_HIGHBYTE, cal_tps0min);
+  writeIntToEEPROM(tps0max_LOWBYTE, tps0max_HIGHBYTE, cal_tps0max);
+}
+
+void setup() {  
+  #ifdef DEBUG
+  Serial.begin(115200);
+  #endif
+  
   pinMode(app0, INPUT);
   pinMode(app1, INPUT);
   pinMode(tps0, INPUT);
@@ -42,11 +108,11 @@ void setup() {
 
   if(digitalRead(BTN_PIN)) {
     calibration = true;
+  } else {
+    readDataFromEEPROM();
   }
-
-  #ifdef DEBUG
-  Serial.begin(115200);
-  #endif
+    
+  timerInitialisation();
 }
 
 void calibrate() {
@@ -95,10 +161,8 @@ void calibrate() {
 
   if (millis() > 5000 && digitalRead(BTN_PIN)) {
     digitalWrite(LED_PIN, HIGH);
-    
-    // TODO: write calibration to flash
-    delay(1000);
-    
+    writeCalibrationToEEPROM();
+    delay(5000);
     digitalWrite(LED_PIN, LOW);
   }
 }
